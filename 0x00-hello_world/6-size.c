55a111e42c38a91d6bfc2d513c81997cb9d91dba
khaliyagi <ishaqkhalifa003@gmail.com>
2022-09-11 02:21:22 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
-#!/bin/bash
-)#include <stdio.h>
+#include <stdio.h>
 /**
  *  * main - main block
  *   * Return: 0
  *    */
 int main(void)
 {
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
-							return (0);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
 100)#!/bin/bash
 gcc -S -masm=intel $CFILE
@@ -26,9 +25,9 @@ gcc -S -masm=intel $CFILE
  *      */
 int main(void)
 {
-		write(2,
-					      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-					      	      59);
+	write(2,
+				      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+				      	      59);
 
-			return (1);
-}
+	return (1);
+}}
